f3cd0bd391bb8e4e163f4c9f22cd9ac270cbbbf6
prosper1 <gundotshili@gmail.com>
2020-08-13 21:43:29 +0200

    Added shop component backbones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,80 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './shop/products/products.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { CartComponent } from './shop/cart/cart.component';
+import { PaymentsComponent } from './shop/payments/payments.component';
+import { TrackOrderComponent } from './shop/track-order/track-order.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { AccountComponent } from './account/account.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent,
+  },
+  {
+    path : 'home',
+    component : HomeComponent,
+  },
+  {
+    path : 'shop',
+    component : ProductsComponent,
+  },
+  {
+    path : 'auth',
+    component : AuthenticationComponent
+  },
+  {
+    path : 'cart',
+    component : CartComponent
+  },
+  {
+    path : 'track-order',
+    component : TrackOrderComponent
+  },
+  {
+    path : 'payments',
+    component : PaymentsComponent
+  },
+  {
+    path : 'account',
+    component : AccountComponent
+  },
+  {
+    path : 'faq',
+    component : FaqComponent
+  },
+  {
+    path : 'contact',
+    component : ContactComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const PageRoutes = [
+  AuthenticationComponent,
+  AccountComponent,
+  CartComponent,
+  ContactComponent,
+  FaqComponent,
+  ProductsComponent,
+  PaymentsComponent,
+  ProductDetailsComponent,
+  HomeComponent,
+  TrackOrderComponent,
+
+]
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, PageRoutes } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    NavComponent,
+    FooterComponent,
+    AboutComponent,
+    ServicesComponent,
+    PageRoutes,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
