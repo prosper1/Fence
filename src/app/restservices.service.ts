a421729e6f6fc063665ecc256d6a8b7c7e6705b9
prosper1 <gundotshili@gmail.com>
2020-09-29 02:45:53 +0200

    Fixed auth service

--- src/app/restservices.service.ts
+++ src/app/restservices.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap, map } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
-const apiUrl = 'http://localhost:8000/';
+const apiUrl = 'http://www.khonis.co.za/';
 
 
 @Injectable({
@@ -15,10 +17,29 @@ export class RestservicesService {
 
   constructor(private http: HttpClient) { }
 
-  login(logins: any) {
+  login(logins): Observable<any> {
     const url = apiUrl + 'rest-auth/login/';
     return this.http.post(url, logins, httpOptions).pipe(
       tap(_ => console.log(`login success`)),
     );
+  }
+
+  register(accountInfo): Observable<any> {
+    const url = apiUrl + 'rest-auth/login/';
+    return this.http.post(url, accountInfo, httpOptions).pipe(
+      tap(_ => console.log(`login success`)),
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    console.log(message);
   }
 }
