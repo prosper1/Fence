1e4180f3eb59045acdf13e21a466ba33c6048d14
gundotshili@gmail.com <gundotshili@gmail.com>
2020-10-01 19:57:44 +0200

    added product service

--- src/app/_services/shop.service.ts
+++ src/app/_services/shop.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = 'http://www.khonis.co.za/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  products(): Observable<any> {
+    const url = apiUrl + 'apis';
+    return this.http.get(url + '/products', httpOptions).pipe(
+      tap(_ => console.log('fetch products'))
+    );
+  }
+
 }
